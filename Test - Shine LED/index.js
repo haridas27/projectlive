cac074ac40d0a29d4319d2cceef0c7670fd32841
haridas27 <130338249+haridas27@users.noreply.github.com>
2024-05-17 12:09:51 +0530

    project

---
+++ Test - Shine LED/index.js
@@ -0,0 +1,8 @@
+// Initialization for ES Users
+// import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+
+// initMDB({ Dropdown, Collapse });
+$('.carousel').carousel({
+    interval: 3000,
+    pause: false
+  })
